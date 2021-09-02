5b198ca04cf33e74055b27400e7c1bdca5810920
opencpv <sulemanaadams375@gmail.com>
2021-09-02 17:49:00 +0000

    rename.py updated

--- rename_files.py
+++ rename_files.py
@@ -19,28 +19,28 @@ for file in files:
     file_directory = data_dir+file
     file_name = file.split('_')[0].lower()
     if file_name=='yes':
-        new_name = "yes"+str(counter)+'.ogg'
+        new_name = "yes_"+str(counter)+'.ogg'
         new_dir = new_data_dir+new_name
         shutil.copy(file_directory,new_data_dir)
         os.rename(new_data_dir+file,new_dir)
         PrintProgress(progress,total_file_num)
         progress=progress+1
     elif file_name=='no':
-        new_name = "no"+str(counter)+'.ogg'
+        new_name = "no_"+str(counter)+'.ogg'
         new_dir = new_data_dir+new_name
         shutil.copy(file_directory,new_data_dir)
         os.rename(new_data_dir+file,new_dir)
         PrintProgress(progress,total_file_num)
         progress=progress+1
     elif file_name=='on':
-        new_name = "on"+str(counter)+'.ogg'
+        new_name = "on_"+str(counter)+'.ogg'
         new_dir = new_data_dir+new_name
         shutil.copy(file_directory,new_data_dir)
         os.rename(new_data_dir+file,new_dir)
         PrintProgress(progress,total_file_num)
         progress=progress+1
     else:
-        new_name = "off"+str(counter)+'.ogg'
+        new_name = "off_"+str(counter)+'.ogg'
         new_dir = new_data_dir+new_name
         shutil.copy(file_directory,new_data_dir)
         os.rename(new_data_dir+file,new_dir)
